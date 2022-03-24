8ab574ed8d19a8590a8ca7cba7134e99148c4cf8
jieunsys <jieunsys@naver.com>
2022-03-24 10:45:01 +0900

    init

---
+++ src/main/java/hello104/Controller.java
@@ -0,0 +1,12 @@
+package hello104;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	@RequestMapping(value="/")
+	public String hello104() {
+		return "hello104 1234 !@#$";
+	}
+}
